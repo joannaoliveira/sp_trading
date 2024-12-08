de974b08a34c400f93e44a00cbfacc043f8b4f07
Joanna Oliveira <joannaoliveira44#gmail.com>
2024-12-08 17:24:12 -0300

    Produto

--- assets/js/global.js
+++ assets/js/global.js
@@ -61,6 +61,31 @@ $( document ).ready(function() {
             }
           }
         ]
-      });
+    });
+
+    $('.responsive-produto').slick({
+        dots: true,
+        arrows: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+          {
+            breakpoint: 991,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+    });
 
 });
